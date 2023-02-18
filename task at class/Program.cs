1e0db7a1911c4402057ccf5186f392e6ed41535d
mammadnm <mammadnm@CATR4>
2023-02-18 14:43:36 +0400

    Add project files.

---
+++ task at class/Program.cs
@@ -0,0 +1,202 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+
+namespace task_at_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1.1
+            //Console.WriteLine("sozu daxil edin ");
+            //string txt = Console.ReadLine();
+            //bool check = false;
+
+            //for (int i = 0; i < txt.Length; i++)
+            //{
+            //    if (txt[i] == 'b')
+            //    {
+            //        check = true;
+            //        break;  
+            //    }
+
+
+
+            //}
+
+            //Console.WriteLine(check);
+            #endregion
+
+
+
+
+
+
+            #region Task 1.2 
+
+
+
+            //Console.WriteLine("ededi daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //bool bolunur = false;
+
+
+            //if (n > 0)
+            //{
+            //    if (n % 7 == 0 && n % 3 == 0)
+            //    {
+            //        bolunur= true;
+
+            //        Console.WriteLine("eded 3 ve 7-ye tam bolunur");
+
+            //    }
+            //    else
+            //        Console.WriteLine("eded 3 ve 7-ye tam bolunmur");
+
+
+            //}
+            #endregion
+
+
+
+            #region task 1.3
+            //Console.WriteLine("ededi daxil ed");
+            //int n = int.Parse(Console.ReadLine());
+            //int lastDigit = n % 10;
+            //int i = n * lastDigit;
+            //Console.WriteLine(i);
+
+
+
+
+
+            #endregion
+
+            #region task 1.4
+
+            //Console.WriteLine(" 3 eded daxil ed");
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());
+
+            //int lastDigit_a = a % 10;
+            //int lastDigit_b = b % 10;
+            //int lastDigit_c = c % 10;
+
+            //lastDigit_a *= lastDigit_a;
+            //lastDigit_b *= lastDigit_b;
+            //lastDigit_c *= lastDigit_c;
+
+
+            //int result = lastDigit_a + lastDigit_b + lastDigit_c;
+
+            //Console.WriteLine(result);
+
+            #endregion
+
+
+            #region task 1.5
+
+            //Console.WriteLine("4 reqemli ededi daxil ed");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int a = 1;
+            //int b = 0;
+
+            //if (n > 999 && n < 10000)
+            //{
+            //    while (n > 0)
+            //    {
+            //        int num = n % 10;
+            //        if (num % 2 == 0)
+
+            //        {
+
+            //        }
+            //        else
+            //        {
+            //            b += num;
+
+            //        }
+
+            //        n -= num;
+            //        n /= 10;
+
+            //    }
+
+
+
+            //    Console.WriteLine($"{a} , {b}");
+            //}
+            //else
+            //    Console.WriteLine("eded 4 reqemli deyil");
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region task 1.6
+
+
+            //int[] arr = new int[] { 4, 2, 6, 7, 1, 9, 3, 7, 2 };
+            //int cut = 0;
+            //int tek = 1;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        cut += arr[i];
+            //    }
+            //    else
+            //    {
+            //        tek *= arr [i];
+
+            //    }
+
+
+            //}
+            //Console.WriteLine($"{tek}  ,  {cut}");
+
+
+
+
+
+            #endregion
+
+
+            #region Task 2.1
+
+
+            //int a = 3;
+            //int b = 4;
+            //int c = 5;
+            //    if (a + b > c && a + c > b && b + c > a)
+            //{
+
+            //    Console.WriteLine("ucbucaqlidir");
+            //}
+            //else
+            //    Console.WriteLine("ucbucaqli deyil");
+
+            #endregion
+
+
+            #region task 2.2
+
+
+
+
+
+
+
+        }
+
+
+    }
+}             
